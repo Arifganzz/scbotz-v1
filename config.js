fe63e365ff730274ab2c03a43b77407b90d5dda6
Arifbotzzz <113294356+Arifganzz@users.noreply.github.com>
2022-09-26 12:24:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['60189830350', '60149431385', '60189830350'] // Masukan nomot kalian
-global.mods = ['60189830350', '60149431385'] // Moderator
+global . pemilik   = [ '+6281770887696'
+                      , '60149431385' , '60189830350' ] // Pasukan nomot kalian
+global . mod  = [ '60189830350' , '60149431385' ] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
